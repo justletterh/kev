f47f89e00b276e3de434c8f69ca25def003ab682
justletterh <63440442+justletterh@users.noreply.github.com>
2023-10-01 22:46:08 -0500

    h

---
+++ main.py
@@ -0,0 +1,106 @@
+import nextcord as discord
+#import onami
+from owo import owoify
+from tools import meth, get_settings
+from nextcord.ext import commands
+from nextcord import Interaction
+from onami.features.baseclass import Feature
+from onami.cog import OPTIONAL_FEATURES, STANDARD_FEATURES
+
+#never ever post the token
+
+bot=get_settings("settings.json")
+
+intents = discord.Intents.all()
+intents.message_content = True
+
+class MegaBot(commands.Bot):
+    async def is_owner(self, user):
+        if user.id in bot.owners:
+            return True
+        else:
+            return False
+
+client = MegaBot(command_prefix=bot.pfx,case_insensitive=True,intents=intents,help_command=None)
+
+class Onih(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
+    @Feature.Command(parent="oni", name="js")
+    async def oni_js(self, ctx):
+        await ctx.send("h")
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    await client.change_presence(status=bot.status.value,activity=discord.Game(bot.status.text))
+
+@client.slash_command(name="h", description='Responds with "h"', guild_ids=bot.guilds)
+async def h(interaction):
+    await interaction.response.send_message("h")
+
+@client.slash_command(name="ping", description='Responds with "Pong!"', guild_ids=bot.guilds)
+async def ping(interaction):
+    await interaction.response.send_message("Pong!")
+
+@client.slash_command(name="pong", description='Responds with "Ping!"', guild_ids=bot.guilds)
+async def pong(interaction):
+    await interaction.response.send_message("Ping!")
+
+@client.slash_command(name= "calc", description= "This command runs math!", guild_ids= bot.guilds)
+async def calc(interaction, math):
+    try:
+        await interaction.response.send_message(meth(math))
+    except:
+        await interaction.response.send_message(f"Could not evaluate `{math}` :(")
+
+@client.slash_command(name= "owo", description= "This command owoifies text!", guild_ids= bot.guilds)
+async def owo(interaction, text):
+    await interaction.response.send_message(owoify(text))
+
+@client.slash_command(name= "help", description= "This command displays the help message for this bot!", guild_ids= bot.guilds)
+async def help(interaction):
+    e=discord.Embed(title=f"{bot.name} Help", color=bot.color, description="This is a list of all my commands and how to use them :)")
+    e.set_footer(text="Made By Kev And H")
+    e.add_field(name="H",value="This command responds with h and is used like `/h`")
+    e.add_field(name="Calc",value="This command calculates math and is used like `/calc 1+1`")
+    e.add_field(name="Ping",value="This command responds with pong and is used like `/ping`")
+    e.add_field(name="Pong",value="This command responds with ping and is used like `/pong`")
+    e.add_field(name="owo",value=owoify("This command owoifies the text you give it")+" and is used like `/owo Hello, World!!`")
+    e.add_field(name="Help",value="this command displays this message and is used like `/help`")
+    await interaction.response.send_message(embed=e)
+
+@client.slash_command(name="test", description="Sends an anonymous confession!", guild_ids=bot.guilds)
+async def test(interaction, text):
+    channel = client.get_channel(bot.anon_channel)
+    if bot.anon_log:
+        log=client.get_channel(bot.anon_log_channel)
+    try:
+        e=discord.Embed(description=text, color=bot.color)
+        e.set_footer(text=f"Anonymous confession by {bot.name}")
+        msg= await channel.send(embed=e)
+        if bot.anon_log:
+            try:
+                le=discord.Embed(title=f"Anonymous confession sent by {interaction.user.name}", description=f'{interaction.user.mention} said "{text}"', color=bot.color)
+                le.set_footer(text=f"{interaction.user.name.capitalize()}'s id is {interaction.user.id}")
+                await log.send(embed=le)
+            except:
+                le=discord.Embed(title=f"Anonymous confession sent by {interaction.user.name}", description=f"{interaction.user.mention}'s confession is {msg.jump_url}", color=bot.color)
+                le.set_footer(text=f"{interaction.user.name.capitalize()}'s id is {interaction.user.id}")
+                await log.send(embed=le)
+        await interaction.response.send_message(f"Sent in {channel.mention}!", ephemeral= True)
+    except:
+        await interaction.response.send_message("Message was too long :(", ephemeral= True)
+        if bot.anon_log:
+            le=discord.Embed(title=f"Anonymous confession attempted by {interaction.user.name}", description= f"But {interaction.user.mention}'s confession was too long :(", color=bot.color)
+            le.set_footer(text=f"{interaction.user.name.capitalize()}'s id is {interaction.user.id}")
+            await log.send(embed=le)
+            try:
+                await log.send(f"{interaction.user.name.capitalize()}'s confession was: ```\n{text}\n```")
+            except:
+                pass
+
+@client.event
+async def on_command_error(ctx, error):
+    await ctx.send(f"You did something wrong.\n```\n{error}\n```")
+
+client.add_cog(Onih(bot=client))
+client.run(bot.token)
---
+++ owo.py
@@ -0,0 +1,82 @@
+'''
+Introducing you to the OwOpy module!\n
+This module suits your OwOify needs perfectly, and it\'s customizable as well!\nrams and applications.\n
+It has 1 function - owoify()\n
+The module\'s github link - https://github.com/Nimboss2411/OwOpy
+'''
+"""
+MIT License
+
+Copyright (c) 2021 Nimboss2411
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+#Modules
+from random import choice
+
+#Global variable initialisation
+owo_vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+owo_emotes = ['^w^', '>w< ', 'UwU ', '(・`ω\\´・)', 
+    '(´・ω・\\`)', 'OwO', '◔w◔ ', '( ͡o ω ͡o )', '(O꒳O)', 
+    '( °ω° )', '( ͡o ꒳ ͡o )', 'ღ(O꒳Oღ)', 
+    '*𝓷𝓾𝔃𝔃𝓵𝓮𝓼 𝔂𝓸𝓾𝓻 𝓬𝓱𝓮𝓼𝓽*', '‿︵*𝓇𝒶𝓌𝓇*‿︵ ʘwʘ', 
+    '✼ ҉♡ (。O⁄ ⁄ω⁄ ⁄ O。) ҉♡ ✼', '✧･ﾟ: *✧･ﾟ:*(OwO)*:･ﾟ✧*:･ﾟ✧', 
+    'ᎧᏇᎧ', '♡w♡', 'ÒwÓ', '≧◡≦', '✧(˘•ω•˘)ง', '~(˶‾᷄ꈊ‾᷅˵)~', 
+    'ᕙ(⇀w↼‶)ᕗ', '༼ ༎ຶ w ༎ຶ༽', '( ͡° w ͡°)', '(•w•)', '♤w♤', '♧w♧', 
+    '(๑و•̀ω•́)و', '(˶‾᷄ ⁻̫ ‾᷅˵)', '꒰๑´•.̫ • `๑꒱', '・ω・', '>ω^', 
+    '{・ω-*}', 'ˁ(⦿@ᴥ⦿*)ˀ', 'ʕ✿๑•́ ᴥ •̀๑✿ʔ', '(•̯͡.•̯͡)', '꒰◍ᐡᐤᐡ◍꒱', 
+    '༼ (´・ω・`) ༽', '♥(⇀ᆽ↼ﾐ)∫', '✿(≗ﻌ≗^)', '( ͒ ඉ .̫ ඉ ͒)', 
+    '( ^◡^)', '^•ﻌ•^', '{ @❛ꈊ❛@ }', '^•ﻌ•^ฅ', '(✿^U^)/', 
+    '(≗ﻌ≗^)']
+__version__ = '0.0.2'
+
+def owoify(owo_string: str, level: int = 3):
+    '''
+    OwOifies (and UwUifies) your text!\n
+    Usage - owopy.owoify('string', level) #level defaults to 2\n
+    How levels work -\n
+    There are three levels - 1, 2 and 3.\n
+    The higher the level the more OwOified your text will be, try it out!
+    '''
+
+    if not isinstance(owo_string, str): raise ValueError('n...nani?! the sentence awgument must be a stwing!')
+    if not isinstance(level, int): raise ValueError('b...baka?! the level awgument must be a non-decimal numbew!')
+    if not level in [1, 2, 3]: raise ValueError('o...owo?! the level awgument must be between 1 and thwee (1 and 3 incwuded!)')
+    
+    owo_string = owo_string.replace('r', 'w').replace('R', 'W').replace('l', 'w').replace('L', 'W').replace('b', 'bw').replace('B', 'BW')
+    
+    if level == 3:
+        owo_string = owo_string.replace('o', 'owo').replace('O', 'OwO').replace('!', f'! {choice(owo_emotes)}{choice(owo_emotes)}').replace('?', f'? {choice(owo_emotes)}{choice(owo_emotes)}').replace('.', f'{choice(owo_emotes)}{choice(owo_emotes)}')
+    elif level == 2:
+        owo_string = owo_string.replace('!', f'! {choice(owo_emotes)}').replace('?', f'? {choice(owo_emotes)}').replace('.', f'{choice(owo_emotes)}')
+
+    for vowel in owo_vowels:
+        if f'n{vowel}' in owo_string: owo_string = owo_string.replace(f'n{vowel}', f'ny{vowel}')
+        elif f'N{vowel}' in owo_string: owo_string = owo_string.replace(f'N{vowel}', f'NY{vowel}')
+        
+    for vowel in owo_vowels:
+        if f'b{vowel}' in owo_string: owo_string = owo_string.replace(f'b{vowel}', f'bw{vowel}')
+        elif f'B{vowel}' in owo_string: owo_string = owo_string.replace(f'B{vowel}', f'BW{vowel}')
+    
+    if level == 2: owo_string = f'{choice(owo_emotes)} {owo_string} {choice(owo_emotes)}'
+    elif level == 3: owo_string = f'{choice(owo_emotes)} {choice(owo_emotes)} {owo_string} {choice(owo_emotes)} {choice(owo_emotes)}'
+    
+    return owo_string
